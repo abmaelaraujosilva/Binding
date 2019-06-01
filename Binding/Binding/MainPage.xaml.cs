6cd1daf8f82eb90917096de44ac700a69ee98ff9
abmaelaraujosilva <abmaelsilva02@outlook.com>
2019-06-01 09:56:34 -0300

    Add project files.

---
+++ Binding/Binding/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Binding
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MensageViewModel();
+        }
+    }
+    public class MensageViewModel:INotifyPropertyChanged
+    {
+        string mensagem;
+        public string Mensagem
+        {
+            get
+            {
+                return mensagem;
+            }
+            set
+            {
+                if(mensagem != value)
+                {
+                    mensagem = value;
+                    OnPropertyChanged("Mensagem");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var Changed = PropertyChanged;
+            if(Changed != null)
+            {
+                PropertyChanged(this,
+                    new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        //public MensageViewModel()
+        //{
+        //    this.Mensagem = "Ola Mundo";
+        //    this.LabelMsg = "Ola Rai Mundo";
+        //}
+    }
+}
